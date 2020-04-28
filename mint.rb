07eb4b5e25e8a00700e3ed63e833113d46c4e166
unive <universato@gmail.com>
2020-04-28 12:33:20 +0900

    fix plus & minus

--- mint.rb
+++ mint.rb
@@ -5,12 +5,22 @@ class Integer
   @@mod = 10 ** 9 + 7
   @@h = Hash.new
 
-  def +@(other)
-    super(other) % @@mod
+  def +@
+    super % @@mod
   end
 
-  def -@(other)
-    super(other) % @@mod
+  def -@
+    super % @@mod
+  end
+
+  alias_method :primitive_plus, :+
+  def +(other)
+    res = primitive_plus(other) % @@mod
+  end
+
+  alias_method :primitive_minus, :-
+  def -(other)
+    primitive_minus(other) % @@mod
   end
 
   alias_method :primitive_times, :*
@@ -18,6 +28,7 @@ class Integer
     self.primitive_times(other) % @@mod
   end
 
+  #alias_method :primitive_div, :/
   def /(other)
     @@h[other] ||= other.pow(@@mod-2, @@mod)
     self * @@h[other]
@@ -65,3 +76,37 @@ ans = (k..n+1).map{|i| i * (n + n - i + 1) / 2 - i * ( i - 1 ) / 2 + 1 }.sum
 puts ans
 # p 1.class.instance_methods
 # p 1.class.superclass.instance_methods
+
+# require 'minitest/autorun'
+
+# class Mint_Test < Minitest::Test
+
+#   m = 10 ** 9 + 7
+#   def test_plus
+#     m = 10 ** 9 + 7
+#     assert_equal 0, 1_000_000_007  + 0
+#     assert_equal 0, 0 + 1_000_000_007
+#     assert_equal 1, 1_000_000_007  + 1
+#     assert_equal 1, 1 + 1_000_000_007
+#     assert_equal 1, 2 + 1_000_000_006
+#     assert_equal 1, 1_000_000_006  + 2
+#     assert_equal 0, 1 + 1_000_000_006
+#     assert_equal 0, 1_000_000_006 + 1
+#     assert_equal 1, +1
+#     assert_equal 0, +0
+#   end
+#   def test_minus
+#     m = 10 ** 9 + 7
+#     assert_equal 1_000_000_006, -1
+#     assert_equal -1, 1_000_000_006
+#     assert_equal 1_000_000_007, -0
+#     assert_equal -(1_000_000_007), 0
+#   end
+#   def test_equal
+#     m = 10 ** 9 + 7
+#     assert 0 == 10 ** 9 + 7
+#     assert 1 == 10 ** 9 + 8
+#     assert 2 != 3
+#     assert 50 * m == 30 * m
+#   end
+# end
