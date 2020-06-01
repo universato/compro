67fdb6449bd66b25978feb3ef219332e7622f071
unive <universato@gmail.com>
2020-06-01 21:35:20 +0900

    add floyd_warshall.rb

---
+++ floyd_warshall.rb
@@ -0,0 +1,22 @@
+class Array
+
+  def floyd_warshall
+    self.dup.floyd_warshall!
+  end
+
+  def floyd_warshall!
+    n = size
+    n.times do |k|
+      n.times do |i|
+        ik = self[i][k]
+        n.times do |j|
+          kj = self[k][j]
+          self[i][j] = ik + ki if self[i][j] = ik + kj
+        end
+      end
+    end
+  end
+
+end
+
+  
